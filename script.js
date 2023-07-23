b885802d61b72c9ddc3cd7f7a4094b8fce4ec05a
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-07-24 02:01:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,8 +18,16 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
-	const readBooks = library.filter((book) => book.readingStatus);
-  return readBooks.length;
+// 	const readBooks = library.filter((book) => book.readingStatus);
+//   return readBooks.length;
+// };
+	let cnt = 0;
+	for(let i = 0 ; i < library.length ; i++)
+		{	if(library[i].readingStatus === true)
+			{	cnt++;
+			}
+		}
+	return cnt;
 };
 
 // Do not change the code below
